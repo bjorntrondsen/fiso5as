026098080bd298d46e4bb68fbad86b05fefc16e2
Sharagoz <contact@sharagoz.com>
2013-08-25 18:00:35 +0200

    scheduler only syncs active matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -7,6 +7,10 @@ class Match < ActiveRecord::Base
   belongs_to :away_team, class_name: 'Team'
   has_many :h2h_matches, dependent: :destroy
 
+  def self.active
+    self.where(["starts_at < ? AND ends_at > ?", Time.zone.now, Time.zone.now])
+  end
+
   def fpl_sync
     puts "Getting FPL data (match #{self.id}) #{Time.zone.now}"
     transaction do
--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -3,7 +3,7 @@ if Rails.env == 'production'
   scheduler.every '5m', blocking: true do
     # Runs from 13:00- 23.59
     if Time.zone.now.hour > 12
-      GameweeksController.get_matches
+      Match.active.each{|m| m.fpl_sync }
     end
   end
 end
