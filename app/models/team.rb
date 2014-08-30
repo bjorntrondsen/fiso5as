6f41c925621673df1c2bc569571482b42e0e5a70
Sharagoz <contact@sharagoz.com>
2014-08-30 14:44:38 +0200

    link to team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 class Team < ActiveRecord::Base
   has_many :managers, dependent: :destroy
   validates_presence_of :name, :fpl_id
+
+  def fpl_url
+    "http://fantasy.premierleague.com/my-leagues/#{fpl_id}/standings/"
+  end
 end
