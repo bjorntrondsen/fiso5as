19c314cde638a5eb9f95579464729b8dcea22cf3
Sharagoz <contact@sharagoz.com>
2015-10-18 12:10:53 +0200

    fix duplication

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,7 +2,7 @@ if Rails.env == 'production'
   scheduler = Rufus::Scheduler.new
   scheduler.every '5m', blocking: true do
     if Time.zone.now.hour > 10 # Dont run at night
-      Match.active.each{|m| m.fpl_sync }
+      Match.sync_all
     end
   end
 end
