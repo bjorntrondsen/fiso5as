8a09f18384c95eff928cae702e2357243b517834
Sharagoz <contact@sharagoz.com>
2017-08-20 02:02:07 +0200

    Sync every 2 mins

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
   scheduler = Rufus::Scheduler.new
-  scheduler.every '5m', blocking: true do
+  scheduler.every '2m', blocking: true do
     if Time.zone.now.hour > 10 # Dont run at night
       Match.sync_all
     end
