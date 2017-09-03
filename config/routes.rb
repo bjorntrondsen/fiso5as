b28ce0892e5433bacba56f4e3f5613560859da2e
Sharagoz <contact@sharagoz.com>
2017-09-04 00:51:12 +0200

    Add admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Fiso5as::Application.routes.draw do
   resources :matches, only: %i( index show new create )
-  resources :game_weeks, only: %i( index )
+  get '/admin' => 'game_weeks#index'
+
   get '/:access_key' => 'matches#index', as: :gw
   root to: "matches#index"
 end
