19a8baec843da1dd7ea264dea5209d5885757077
Sharagoz <contact@sharagoz.com>
2017-09-09 08:29:11 +0200

    Rufus blocking test

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,8 +1,11 @@
 if Rails.env == 'production'
   scheduler = Rufus::Scheduler.new
-  scheduler.every '2m', blocking: true do
-    if Time.zone.now.hour > 10 # Dont run at night
-      Match.sync_all
-    end
+  #scheduler.every '2m', blocking: true do
+    #if Time.zone.now.hour > 10 # Dont run at night
+      #Match.sync_all
+    #end
+  #end
+  scheduler.every '10s', blocking: true do
+    sleep(5)
   end
 end
