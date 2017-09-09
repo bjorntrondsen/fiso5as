8ead5a9411c2b62ccfecafd89af032fb21c1b792
Sharagoz <contact@sharagoz.com>
2017-09-09 08:49:06 +0200

    Revert "Rufus blocking test"
    This reverts commit 19a8baec843da1dd7ea264dea5209d5885757077.

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,11 +1,8 @@
 if Rails.env == 'production'
   scheduler = Rufus::Scheduler.new
-  #scheduler.every '2m', blocking: true do
-    #if Time.zone.now.hour > 10 # Dont run at night
-      #Match.sync_all
-    #end
-  #end
-  scheduler.every '10s', blocking: true do
-    sleep(5)
+  scheduler.every '2m', blocking: true do
+    if Time.zone.now.hour > 10 # Dont run at night
+      Match.sync_all
+    end
   end
 end
