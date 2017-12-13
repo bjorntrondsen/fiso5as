ed6bbfd447c318160b21d6148da37415277721ce
Sharagoz <contact@sharagoz.com>
2017-12-14 00:30:26 +0100

    RUn 24/7

--- config/schedule.rb
+++ config/schedule.rb
@@ -26,6 +26,6 @@
 job_type :runner_with_lock, "cd :path && flock -n /var/lock/:lock.lock bin/rails runner -e :environment ':task' :output"
 
 # Runs every 2 minutes from 10:00 to 23:00
-every '*/2 10-23 * * *' do
+every '*/2 * * * *' do
   runner_with_lock "GameWeek.sync_open", lock: 'job_runner_fiso5as'
 end
